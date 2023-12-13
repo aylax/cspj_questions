d2c16f92737a2418992f67a910f1bd758c63a867
aylax.zhou <zhoubye@foxmail.com>
2023-12-14 01:43:52 +0800

    new :: cspj -> queue question of maximal rectangle

--- solutions/solutions.cc/structure/stacks/max_area_rect2.cc
+++ solutions/solutions.cc/structure/stacks/max_area_rect2.cc
@@ -1,5 +1,5 @@
 // --------------------------------------------------------
-// File: :最大的矩形面积问题
+// File: :最大的矩形面积2问题
 // Require: __cplusplus >= 201402
 // --------------------------------------------------------
 // Author: zbye
@@ -12,7 +12,7 @@
 // 预声明
 // stdin redirect testdata/
 #ifndef _TESTDATA_
-#define _TESTDATA_ 0  // 1: Open, 0: Close
+#define _TESTDATA_ 1  // 1: Open, 0: Close
 #endif
 
 // --------------------------------------------------------
@@ -26,28 +26,10 @@ using array = std::vector<int>;
 // 定义对象
 
 // --------------------------------------------------------
-// Fn: 最大的矩形面积问题
-// 题解: 单调栈
-void max_area_rect() {
-#if _TESTDATA_
-  // set "../testdata/.../xxx.in -> stdin
-  freopen("../testdata/structure/max_area_rect.in", "r", stdin);
-#endif
-
-  usize n;  // 元素个数 n
-  std::cin >> n;
-  array arr(n);  // n个长方形高度
-
-  for (usize i = 0; i < n; i++) {
-    std::cin >> arr[i];
-  }
-
-  // close stdin
-#if _TESTDATA_
-  fclose(stdin);
-#endif
-
+// Fn: 直方图内最大矩形面积
+value maxrect(const array &arr) {
   stack ms;  // 单调栈
+  usize n = arr.size();
 
   // Lambda: 求弹出区域的最大矩形面积
   auto max_included = [&ms, &arr](usize r, value k) -> value {
@@ -77,6 +59,55 @@ void max_area_rect() {
   ans = std::max(ans, loc);
 
   // 输出结果
+  return ans;
+}
+
+// Fn: 最大的矩形面积2问题
+// 题解: 将问题转化为<直方图内求矩形面积>
+// 依次生成每一行直方图, 并计算该行最大矩形面积 Loc
+// 最终取所有行的最大值 ans = Max(Loc)
+// 每一行直方图 H(R)生成方式:
+//    如果: n2d[R][C] 值为 1,可以与上一行直方图 H(R-1)[C]连通
+//    IF: n2d[R][C] == 1:
+//        H(R)[C] = H(R-1)[C] + 1
+//    否则: 值为 0, 表示无法连通
+//    ELSE:
+//        H(R)[C] = 0
+void max_area_rect2() {
+#if _TESTDATA_
+  // set "../testdata/.../xxx.in -> stdin
+  freopen("../testdata/structure/max_area_rect2.in", "r", stdin);
+#endif
+
+  usize n, m;  // n 行, m 列
+  std::cin >> n >> m;
+
+  // 定义2维数组
+  std::vector<array> n2d(n, array(m));
+
+  // 读入数据
+  for (usize r = 0; r < n; r++) {
+    for (usize c = 0; c < m; c++) {
+      std::cin >> n2d[r][c];
+    }
+  }
+
+  // close stdin
+#if _TESTDATA_
+  fclose(stdin);
+#endif
+
+  array h(m);  // 直方图
+  value loc = 0, ans = 0;
+
+  for (usize r = 0; r < n; r++) {
+    for (usize c = 0; c < m; c++) {
+      bool addable = n2d[r][c] == 1;
+      h[c] = addable ? h[c] + 1 : 0;
+    }
+    loc = maxrect(h);
+    ans = std::max(ans, loc);
+  }
   std::cout << ans << "\n";
 }
 
@@ -84,6 +115,6 @@ void max_area_rect() {
 // Fn: Main
 int main(int argc, char const *argv[]) {
   // std::cout << "c++ standard version: " << __cplusplus << "\n";
-  max_area_rect();
+  max_area_rect2();
   return 0;
 }
