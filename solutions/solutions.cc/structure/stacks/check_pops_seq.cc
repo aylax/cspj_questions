a8f31d38fd0b9b7292389ffc907bc1ab22eca8ba
aylax.zhou <zhoubye@foxmail.com>
2023-12-14 13:42:16 +0800

    new :: cspj -> add stack questions of mock stack

---
+++ solutions/solutions.cc/structure/stacks/check_pops_seq.cc
@@ -0,0 +1,90 @@
+// --------------------------------------------------------
+// File: 验证栈序列问题
+// Require: __cplusplus >= 201402
+// --------------------------------------------------------
+// Author: zbye
+// --------------------------------------------------------
+#include <iostream>
+#include <stack>
+#include <vector>
+
+// --------------------------------------------------------
+// 预声明
+// stdin redirect testdata/
+#ifndef _TESTDATA_
+#define _TESTDATA_ 0  // 1: Open, 0: Close
+#endif
+
+// --------------------------------------------------------
+// 设定类型别名
+using usize = int;
+using value = int;
+using array = std::vector<value>;
+using stack = std::stack<value>;
+
+// --------------------------------------------------------
+// 定义对象
+
+// --------------------------------------------------------
+// Fn: 验证栈序列问题
+// 题解: 模拟出栈匹配思路
+void check_pops_seq() {
+#if _TESTDATA_
+  // set "../testdata/.../xxx.in -> stdin
+  freopen("../testdata/structure/check_pops_seq.in", "r", stdin);
+#endif
+
+  usize n;  // 询问次数
+  std::cin >> n;
+
+  // 读取数据
+  auto readin = [](array &arr, usize len) {
+    // 读取入栈序列
+    for (usize i = 0; i < len; i++) {
+      std::cin >> arr[i];
+    }
+  };
+
+  for (usize i = 0; i < n; i++) {
+    usize len;
+    std::cin >> len;
+
+    // 读取入栈序列
+    array seqin(len);
+    readin(seqin, len);
+
+    // 读取出栈序列
+    array seqout(len);
+    readin(seqout, len);
+
+    stack s;           // 模拟入栈和出栈
+    usize topout = 0;  // 指向出栈序列栈顶
+
+    for (usize topin = 0; topin < len; topin++) {
+      s.push(seqin[topin]);
+      auto mustdo = [&s, &topout, &len]() -> bool {
+        return !s.empty() && topout < len;
+      };
+      while (mustdo() && s.top() == seqout[topout]) {
+        topout += 1;
+        s.pop();
+      }
+    }
+
+    auto ans = (topout == len) ? "Yes" : "No";
+    std::cout << ans << "\n";
+  }
+
+  // close stdin
+#if _TESTDATA_
+  fclose(stdin);
+#endif
+}
+
+// --------------------------------------------------------
+// Fn: Main
+int main(int argc, char const *argv[]) {
+  // std::cout << "c++ standard version: " << __cplusplus << "\n";
+  check_pops_seq();
+  return 0;
+}
